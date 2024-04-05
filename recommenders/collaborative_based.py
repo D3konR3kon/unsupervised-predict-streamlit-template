f3d0d633813105128319084c2db7ae9ebcb1eccd
d3konr3kon <dilavuyo@outlook.com>
2024-04-06 00:45:21 +0200

    updated our style.css file

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -30,20 +30,34 @@
 # Script dependencies
 import pandas as pd
 import numpy as np
+import scipy as sp
+import operator
+
 import pickle
 import copy
 from surprise import Reader, Dataset
 from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from utils.data_loader import load_movie_titles
 
 # Importing data
-movies_df = pd.read_csv('resources/data/movies.csv',sep = ',')
+movies_df = pd.read_csv('resources/data/movies.csv')
 ratings_df = pd.read_csv('resources/data/ratings.csv')
+media_df =  pd.read_csv('resources/data/links_with_media.csv')
+
+
+# merged_df = ratings_df.merge(movies_df, on='movieid', how='left')
+media_df.drop('Unnamed: 0', axis=1)
 ratings_df.drop(['timestamp'], axis=1,inplace=True)
+newMovie_df = movies_df.merge(media_df[['images','link']], on=media_df['movieId'])
+# merged_df
 
 # We make use of an SVD model trained on a subset of the MovieLens 10k dataset.
-model=pickle.load(open('resources/models/SVD.pkl', 'rb'))
+# model=pickle.load(open('resources/models/svd_model.pkl', 'rb'))
+with open('resources/models/am2_unsupervised_recommender.pkl', 'rb') as file:
+    model = pickle.load(file)
+
 
 def prediction_item(item_id):
     """Map a given favourite movie to users within the
@@ -61,7 +75,7 @@ def prediction_item(item_id):
 
     """
     # Data preprosessing
-    reader = Reader(rating_scale=(0, 5))
+    reader = Reader(rating_scale=(0.5, 5))
     load_df = Dataset.load_from_df(ratings_df,reader)
     a_train = load_df.build_full_trainset()
 
@@ -100,49 +114,141 @@ def pred_movies(movie_list):
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
+
 def collab_model(movie_list,top_n=10):
-    """Performs Collaborative filtering based upon a list of movies supplied
-       by the app user.
-
-    Parameters
+    """Short summary.
+    arameters
     ----------
-    movie_list : list (str)
-        Favorite movies chosen by the app user.
+    movie_list : type
+        Description of parameter `movie_list`.
     top_n : type
-        Number of top recommendations to return to the user.
-
+        Description of parameter `top_n`.
     Returns
     -------
-    list (str)
-        Titles of the top-n movie recommendations to the user.
-
+    type
+        Description of returned object.
     """
+    #getting list of ids of 10 users that rated movies highly
+    user_ids = pred_movies(movie_list)
 
-    indices = pd.Series(movies_df['title'])
-    movie_ids = pred_movies(movie_list)
-    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
-    for i in movie_ids :
-        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
-    # Getting the cosine similarity matrix
-    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
+    #obtaining the ratings of the users
+
+    df_init_users = ratings_df[ratings_df['userId'].isin(user_ids)]
+
+    # for i in user_ids:
+    #     df_init_users = df_init_users.append(ratings_df[ratings_df['userId']==i])
+
+    df_init_users=df_init_users.drop_duplicates().reset_index(drop=True)
+    
+    #obtaining movieIds from movie titles
+    movie_ids = []
+    for movie in movie_list:
+        movie_ids.append(int(movies_df['movieId'][movies_df['title']==movie]))
+    
+    #adding new user
+    new_row1 = {'userId':1234567, 'movieId':movie_ids[0], 'rating':5}
+    new_row2 = {'userId':1234567, 'movieId':movie_ids[1], 'rating':5}
+    new_row3 = {'userId':1234567, 'movieId':movie_ids[2], 'rating': 5}
+    df_init_users = df_init_users.append([new_row1,new_row2,new_row3],ignore_index=True)
+
+    #pivot ratings
+    pivot_user = pd.pivot_table(df_init_users,values='rating',columns='userId',index='movieId')
+    pivot_user.fillna(0, inplace=True)
+    
+    pivot_user = pivot_user.apply(lambda x: (x-np.min(x))/(np.max(x)-np.min(x)), axis=1)
+
+    pivot_user_arr =  np.array(pivot_user)
+    
+    #finding movies similarities based on users
+    cosine_sim=cosine_similarity(pivot_user_arr,pivot_user_arr)
+    
+    #mat
+    m_index_list = list(pivot_user.index)
+    df = movies_df[movies_df['movieId'].isin(m_index_list)].reset_index(drop=True)
+
+    #Obtaining indices
+    indices = pd.Series(df['title'])
     idx_1 = indices[indices == movie_list[0]].index[0]
     idx_2 = indices[indices == movie_list[1]].index[0]
     idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
+
     rank_1 = cosine_sim[idx_1]
     rank_2 = cosine_sim[idx_2]
     rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
+
     score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
     score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
     score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-     # Appending the names of movies
+
+    # Appending the names of movies
     listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
     recommended_movies = []
     # Choose top 50
     top_50_indexes = list(listings.iloc[1:50].index)
     # Removing chosen movies
     top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies_df['title'])[i])
+    for i in top_indexes[:10]:
+
+        movieItem = {
+            'title': list(movies_df['title'])[i],
+            'image_url': get_movie_image_url(list(movies_df['title'])[i]),
+            # 'ratings' : get_movie_rating(list(movies_df['title'])[i])
+              #  Get image URL
+            # # 'external_link': get_external_link(newMovie_df.iloc[idx]['title'])  # Get external link
+        }
+        recommended_movies.append(movieItem)
+        print(movieItem)
+
+        
+        # print(get_movie_image_url(newMovie_df.iloc[i]['movieId']))
+        
+   
+
     return recommended_movies
+
+def get_movie_image_url(movie_title):
+    """Get the image URL of a movie based on its title.
+    
+    Parameters
+    ----------
+    movie_title : str
+        Title of the movie.
+    
+    Returns
+    -------
+    str
+        Image URL of the movie.
+    """
+    # Find the image URL for the given movie title
+    row = newMovie_df[newMovie_df['title'] == movie_title]
+    if not row.empty:
+        image_url = row['images'].values[0]
+    else:
+        # If image URL is not available, return a default image URL or handle the case accordingly
+        image_url = 'default_image_url.jpg'
+
+    return image_url
+
+def get_movie_rating(movie_title):
+    """
+    Get the rating of a movie from a DataFrame.
+
+    Parameters:
+        movie_title (str): The title of the movie.
+        ratings_df (pandas.DataFrame): DataFrame containing movie ratings (columns: 'userid', 'movieid', 'timestamp', 'ratings').
+        movies_df (pandas.DataFrame): DataFrame containing movie details (columns: 'movieid', 'title', 'genre').
+
+    Returns:
+        float or None: The rating of the movie if found, otherwise None.
+    """
+    # Merge ratings_df and movies_df on 'movieid' column
+    
+
+    # Check if the movie title exists in the merged DataFrame
+    movie_id = movies_df[movies_df['title'] == movie_title]['movieid']
+    if not movie_id.empty:
+        ratings_for_movie = ratings_df[ratings_df['movieid'] == movie_id.iloc[0]]['ratings']
+        if not ratings_for_movie.empty:
+            return ratings_for_movie.mean()
+    print(f"Movie '{movie_title}' not found in the DataFrame.")
+    return None
